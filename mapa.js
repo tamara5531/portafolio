b5029d2dc4aef253a763fa2c341b5db183546345
tamara5531 <tamaraei@yahoo.es>
2024-03-09 01:49:04 -0300

    cuarto commit

--- mapa.js
+++ mapa.js
@@ -1,12 +1,9 @@
-// Función de inicialización del mapa
 function initMap() {
-    // Inicializa el mapa con una ubicación predeterminada
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -33.374759674072266, lng: -70.64069366455078 },
         zoom: 14
     });
 
-    // Obtiene la ubicación del usuario
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             var userLocation = {
@@ -14,26 +11,21 @@ function initMap() {
                 lng: position.coords.longitude
             };
 
-            // Crea un marcador en la ubicación del usuario
             var marker = new google.maps.Marker({
                 position: userLocation,
                 map: map,
                 title: 'Tu ubicación'
             });
 
-            // Centra el mapa en la ubicación del usuario
             map.setCenter(userLocation);
         }, function() {
-            // Maneja los errores de geolocalización
             handleLocationError(true, map.getCenter());
         });
     } else {
-        // El navegador no soporta geolocalización
         handleLocationError(false, map.getCenter());
     }
 }
 
-// Maneja los errores de geolocalización
 function handleLocationError(browserHasGeolocation, pos) {
     var infoWindow = new google.maps.InfoWindow();
     infoWindow.setPosition(pos);
